749394255f9da13b5df07e952fda20d3196d78f3
Sotchi <sovanpisothyan@example.com>
2026-06-03 14:56:03 +0700

    navBar-layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Button from "./components/ui/Button";
+import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Routes>
         <Route></Route>
       </Routes>
---
+++ client/src/components/layout/NavBar.jsx
@@ -0,0 +1,47 @@
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="flex justify-between items-center px-15 py-5 border-b border-default bg-transparent">
+      <div className="flex gap-15 items-center">
+        {/*Logo*/}
+        <div>
+          <Link to="/">
+            <h4 className="font-bold">GitHero</h4>
+          </Link>
+        </div>
+
+        {/*NavLinks*/}
+        <ul className="flex justify-evenly gap-10">
+          <li className="hover:text-gray-300 cursor-pointer">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">
+            <a href="#AboutUs">About us</a>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">
+            <a href="#Features">Feature</a>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">
+            <a href="#FAQs">FAQs</a>
+          </li>
+        </ul>
+      </div>
+
+      {/*Buttons*/}
+      <div className="flex gap-1">
+        <Button onClick={() => navigate("/login")} bcolor="nonoutline">
+          Sign in
+        </Button>
+        <Button onClick={() => navigate("/signup")} bcolor="primary">
+          Sign up
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;

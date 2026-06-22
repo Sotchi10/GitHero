45b287a2c0aa995859e50e8918cd0984a6c4d1c8
Sotchi <sovanpisothyan@example.com>
2026-06-22 15:36:37 +0700

    short fix

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import pfpRoute from "./features/profile/pfpRoute.js";
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials: true
 }));
 

b7bd8902e369bf19097bc36e9aaba8201401d021
Sotchi <sovanpisothyan@example.com>
2026-07-19 15:39:13 +0700

    Deployemnt resolve

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://githero-kappa.vercel.app/",
   credentials: true
 }));
 app.use("/uploads", express.static(path.resolve(__dirname, "../uploads")));
